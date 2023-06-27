23f30472265941b6f6fc19be206fbddf024ef318
KristenM4 <kristen4810@gmail.com>
2023-06-27 11:08:42 +0800

    create AppController and route Index to AppController

---
+++ Controllers/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeaWolf.HR.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
