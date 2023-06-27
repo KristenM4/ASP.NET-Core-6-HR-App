3ab57f38f19bcf9f59a66fc01e9387ebb6fb6685
KristenM4 <kristen4810@gmail.com>
2023-06-27 11:28:14 +0800

    Create employee model class

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeaWolf.HR.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? MiddleName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Position { get; set; }
+    }
+}
