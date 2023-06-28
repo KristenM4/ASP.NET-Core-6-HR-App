33e42e54aa36fad0af2ba50633f836e5a4c381d7
KristenM4 <kristen4810@gmail.com>
2023-06-28 17:54:08 +0800

    Create new DbContext class with Employee DbSet

---
+++ Models/SeaWolfHRDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SeaWolf.HR.Models
+{
+    public class SeaWolfHRDbContext : DbContext
+    {
+        public SeaWolfHRDbContext(DbContextOptions<SeaWolfHRDbContext> 
+            options) : base(options)
+        {   
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
