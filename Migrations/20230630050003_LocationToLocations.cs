26eaf50d84073c73a5fe3fc7cf4566adf2fcc2ca
KristenM4 <kristen4810@gmail.com>
2023-06-30 13:05:12 +0800

    Migration after adding Locations to DbContext, Location table in db renamed to Locations

---
+++ Migrations/20230630050003_LocationToLocations.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeaWolf.HR.Migrations
+{
+    /// <inheritdoc />
+    public partial class LocationToLocations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Location_LocationId",
+                table: "Employees");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Location",
+                table: "Location");
+
+            migrationBuilder.RenameTable(
+                name: "Location",
+                newName: "Locations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Locations",
+                table: "Locations",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Locations_LocationId",
+                table: "Employees",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "LocationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Locations_LocationId",
+                table: "Employees");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Locations",
+                table: "Locations");
+
+            migrationBuilder.RenameTable(
+                name: "Locations",
+                newName: "Location");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Location",
+                table: "Location",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Location_LocationId",
+                table: "Employees",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "LocationId");
+        }
+    }
+}
