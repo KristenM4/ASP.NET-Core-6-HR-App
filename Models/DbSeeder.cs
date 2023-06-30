61390a7b2fbdbc418d0dfe156a62d5d328b8c44e
KristenM4 <kristen4810@gmail.com>
2023-06-30 12:49:30 +0800

    Add a location to DbSeeder

--- Models/DbSeeder.cs
+++ Models/DbSeeder.cs
@@ -8,30 +8,84 @@
                 applicationBuilder.ApplicationServices.CreateScope
                 ().ServiceProvider.GetRequiredService<SeaWolfHRDbContext>();
 
+            if(!context.Locations.Any())
+            {
+                context.Add(
+                    new Location()
+                    {
+                        LocationName = "Farrington Store",
+                        Phone = "8085551234",
+                        AddressLine1 = "84-521 Farrington Highway",
+                        City = "Waianae",
+                        State = "Hawaii",
+                        PostalCode = "96792",
+                        Country = "US"
+                    });
+                context.SaveChanges();
+            }
+
             if (!context.Employees.Any())
             {
-                context.AddRange
-                (
-                new Employee() {FirstName="Bob", LastName="Evans",
-                DateOfBirth=new DateTime(1948, 05, 30), Email="bevans@email.com", Phone="1234567890",
-                Position="Manager"},
+                var farringtonStore = context.Locations.FirstOrDefault(l => l.LocationName == "Farrington Store");
 
-                new Employee() {FirstName="Harland", LastName="Sanders", MiddleName="David",
-                    DateOfBirth=new DateTime(1952, 09, 09), Email="hsanders@email.com", Phone="1234567891",
-                Position="Salesperson"},
+                context.AddRange(
+                new Employee()
+                {
+                    FirstName = "Bob",
+                    LastName = "Evans",
+                    DateOfBirth = new DateTime(1948, 05, 30),
+                    Email = "bevans@email.com",
+                    Phone = "1234567890",
+                    Position = "Manager",
+                    Location = farringtonStore
+                },
 
-                new Employee() {FirstName="Ronald", LastName="McDonald",
-                    DateOfBirth=new DateTime(1965, 11, 25), Email="rmcdonald@email.com", Phone="1234567892",
-                Position="Salesperson"},
+                new Employee()
+                {
+                    FirstName = "Harland",
+                    LastName = "Sanders",
+                    MiddleName = "David",
+                    DateOfBirth = new DateTime(1952, 09, 09),
+                    Email = "hsanders@email.com",
+                    Phone = "1234567891",
+                    Position = "Salesperson",
+                    Location = farringtonStore
+                },
 
-                new Employee() {FirstName="Wendy", LastName="Thomas", MiddleName="Lou",
-                    DateOfBirth=new DateTime(1961, 09, 14), Email="wthomas@email.com", Phone="1234567893",
-                Position="Cashier"},
+                new Employee()
+                {
+                    FirstName = "Ronald",
+                    LastName = "McDonald",
+                    DateOfBirth = new DateTime(1965, 11, 25),
+                    Email = "rmcdonald@email.com",
+                    Phone = "1234567892",
+                    Position = "Salesperson",
+                    Location = farringtonStore
+                },
 
-                new Employee() {FirstName="Forrest", LastName="Raffel", MiddleName="Leroy",
-                    DateOfBirth=new DateTime(1964, 07, 23), Email="fraffel@email.com", Phone="1234567894",
-                Position="Cleaner"}
-                );
+                new Employee()
+                {
+                    FirstName = "Wendy",
+                    LastName = "Thomas",
+                    MiddleName = "Lou",
+                    DateOfBirth = new DateTime(1961, 09, 14),
+                    Email = "wthomas@email.com",
+                    Phone = "1234567893",
+                    Position = "Cashier",
+                    Location = farringtonStore
+                },
+
+                new Employee()
+                {
+                    FirstName = "Forrest",
+                    LastName = "Raffel",
+                    MiddleName = "Leroy",
+                    DateOfBirth = new DateTime(1964, 07, 23),
+                    Email = "fraffel@email.com",
+                    Phone = "1234567894",
+                    Position = "Cleaner",
+                    Location = farringtonStore
+                });
             }
 
             context.SaveChanges();
