b7c0b89a56319196bcbf7bbe97ddbdbcd078a90a
KristenM4 <kristen4810@gmail.com>
2023-06-30 15:36:31 +0800

    register location repository in builder.Services

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<ILocationRepository, LocationRepository>();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<SeaWolfHRDbContext>(options => {
