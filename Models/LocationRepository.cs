ec3689a47a951be00eb8104ad3d404c98c4fd197
KristenM4 <kristen4810@gmail.com>
2023-06-30 12:25:53 +0800

    Create Location Repository

---
+++ Models/LocationRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace SeaWolf.HR.Models
+{
+    public class LocationRepository : ILocationRepository
+    {
+        private readonly SeaWolfHRDbContext _context;
+
+        public LocationRepository(SeaWolfHRDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Location> AllLocations => _context.Locations.OrderBy(l => l.LocationName);
+    }
+}
