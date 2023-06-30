420cb34e001083a53995c00eaeba936453e9bc4e
KristenM4 <kristen4810@gmail.com>
2023-06-30 13:05:47 +0800

    Include Employee's Location foreign key in LINQ queries

--- Models/EmployeeRepository.cs
+++ Models/EmployeeRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace SeaWolf.HR.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SeaWolf.HR.Models
 {
     public class EmployeeRepository : IEmployeeRepository
     {
@@ -9,11 +11,11 @@
             _context = seaWolfHRDbContext;
         }
 
-        public IEnumerable<Employee> AllEmployees => _context.Employees;
+        public IEnumerable<Employee> AllEmployees => _context.Employees.Include(e=>e.Location);
 
         public Employee? GetEmployeeById(int employeeId)
         {
-            return _context.Employees.FirstOrDefault(e => e.EmployeeId == employeeId);
+            return _context.Employees.Include(e => e.Location).FirstOrDefault(e => e.EmployeeId == employeeId);
         }
     }
 }
