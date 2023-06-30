5dc5b861c65f2e9328118281959d5c535591ec95
KristenM4 <kristen4810@gmail.com>
2023-06-30 12:22:18 +0800

    add Location field to Employee model

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -13,5 +13,6 @@ namespace SeaWolf.HR.Models
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Position { get; set; }
+        public Location? Location { get; set; }
     }
 }
