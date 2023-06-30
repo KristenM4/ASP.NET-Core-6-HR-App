8536faa3ba9f430a7301d270259f85d955f9af0c
KristenM4 <kristen4810@gmail.com>
2023-06-30 12:22:45 +0800

    Migration to add Location to db

---
+++ Migrations/20230630041951_AddLocations.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeaWolf.HR.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LocationId",
+                table: "Employees",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    LocationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LocationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AddressLine1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AddressLine2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.LocationId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_LocationId",
+                table: "Employees",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Location_LocationId",
+                table: "Employees",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "LocationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Location_LocationId",
+                table: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_LocationId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Employees");
+        }
+    }
+}
