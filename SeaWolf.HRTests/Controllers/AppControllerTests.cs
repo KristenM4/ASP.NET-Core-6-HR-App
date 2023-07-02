7367d8b3f57224b15b1085f3871e8a78ac854ee9
KristenM4 <kristen4810@gmail.com>
2023-07-02 16:49:52 +0800

    create AppControllerTests and first unit test for Index method

---
+++ SeaWolf.HRTests/Controllers/AppControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeaWolf.HR.Controllers;
+using SeaWolf.HR.ViewModels;
+using SeaWolf.HRTests.Mocks;
+
+namespace SeaWolf.HRTests.Controllers
+{
+    public class AppControllerTests
+    {
+        [Fact]
+        public void Index_Returns_Employees_List()
+        {
+            //setup
+            var mockEmployeeRepository = RepositoryMocks.GetEmployeeRepository();
+            var mockLocationRepository = RepositoryMocks.GetLocationRepository();
+
+            var appController = new AppController(mockEmployeeRepository.Object,
+                mockLocationRepository.Object);
+
+            //act
+            var result = appController.Index();
+
+            //assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var employeeListViewModel = Assert.IsAssignableFrom<EmployeeListViewModel>
+                (viewResult.ViewData.Model);
+            Assert.Equal(5, employeeListViewModel.Employees.Count());
+        }
+    }
+}
