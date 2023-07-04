7d00bcd99f7e0bbe678e6b671a7b4bfba6f5f93d
KristenM4 <kristen4810@gmail.com>
2023-07-04 14:06:40 +0800

    Create new view for employeelist

---
+++ SeaWolf.HR/Views/App/EmployeeList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeaWolf.HR.Views.App
+{
+    public class EmployeeListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
