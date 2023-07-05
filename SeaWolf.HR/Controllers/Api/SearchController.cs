10b7d5853d4cf5fda0a35073cd1c5c4184f48e12
KristenM4 <kristen4810@gmail.com>
2023-07-05 14:04:04 +0800

    create Controllers/Api/ dir and SearchController.cs

---
+++ SeaWolf.HR/Controllers/Api/SearchController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeaWolf.HR.Models;
+
+namespace SeaWolf.HR.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public SearchController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+    }
+}
