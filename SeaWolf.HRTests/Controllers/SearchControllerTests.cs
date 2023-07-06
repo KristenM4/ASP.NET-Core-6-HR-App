0213ab16cc8927e66bc27079c73b63cbc373a7d5
KristenM4 <kristen4810@gmail.com>
2023-07-06 13:05:44 +0800

    create seachcontroller test class

---
+++ SeaWolf.HRTests/Controllers/SearchControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SeaWolf.HR.Models;
+using SeaWolf.HRTests.Mocks;
+
+namespace SeaWolf.HR.Controllers
+{
+    public class SearchControllerTests
+    {
+        private readonly Mock<IEmployeeRepository> _mockEmployeeRepository;
+
+        public SearchControllerTests()
+        {
+            _mockEmployeeRepository = RepositoryMocks.GetEmployeeRepository();
+        }
+    }
+}
