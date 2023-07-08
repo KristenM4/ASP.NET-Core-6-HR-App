24f2e6fd961dcf8f17ebdc98871dc51ebb3f815c
KristenM4 <kristen4810@gmail.com>
2023-07-08 13:20:59 +0800

    change Program.cs to add serilog and remove unneeded comments

--- SeaWolf.HR/Program.cs
+++ SeaWolf.HR/Program.cs
@@ -1,45 +1,60 @@
 using Microsoft.EntityFrameworkCore;
 using SeaWolf.HR.Models;
 using System.Text.Json.Serialization;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-// Add services to the container.
-builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddScoped<ILocationRepository, LocationRepository>();
+try
+{
+    Log.Information("Starting web application");
 
-builder.Services.AddControllersWithViews()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+    builder.Services.AddScoped<ILocationRepository, LocationRepository>();
+
+    builder.Services.AddControllersWithViews()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        });
+
+    builder.Services.AddDbContext<SeaWolfHRDbContext>(options => {
+        options.UseSqlServer(
+            builder.Configuration["ConnectionStrings:SeaWolfHRDbContextConnection"]);
     });
 
-builder.Services.AddDbContext<SeaWolfHRDbContext>(options => {
-    options.UseSqlServer(
-        builder.Configuration["ConnectionStrings:SeaWolfHRDbContextConnection"]);
-});
+    builder.Host.UseSerilog(); // <-- Add this line
 
-var app = builder.Build();
+    var app = builder.Build();
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=App}/{action=Index}/{id?}");
+
+    DbSeeder.Seed(app);
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=App}/{action=Index}/{id?}");
-
-DbSeeder.Seed(app);
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
