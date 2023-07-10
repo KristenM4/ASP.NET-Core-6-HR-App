778d76a1b59af9a1b3acf5f04ba459d7769072e1
KristenM4 <kristen4810@gmail.com>
2023-07-10 10:01:16 +0800

    new unit test for searchemployees

--- SeaWolf.HRTests/Controllers/SearchControllerTests.cs
+++ SeaWolf.HRTests/Controllers/SearchControllerTests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Moq;
+using SeaWolf.HR.Controllers.Api;
 using SeaWolf.HR.Models;
 using SeaWolf.HRTests.Mocks;
 
@@ -8,10 +10,23 @@ namespace SeaWolf.HR.Controllers
     public class SearchControllerTests
     {
         private readonly Mock<IEmployeeRepository> _mockEmployeeRepository;
+        private readonly SearchController _controller;
 
         public SearchControllerTests()
         {
             _mockEmployeeRepository = RepositoryMocks.GetEmployeeRepository();
+            _controller = new SearchController(_mockEmployeeRepository.Object, Mock.Of<ILogger<SearchController>>());
+        }
+
+        [Fact]
+        public void SearchEmployees_Returns_Specific_Employees()
+        {
+            var actionResult = _controller.SearchEmployees("Salesperson$$LastName");
+            var result = actionResult as OkObjectResult;
+            var value = result.Value as IEnumerable<Employee>;
+            
+            Assert.IsType<OkObjectResult>(actionResult);
+            Assert.Equal(2, value.Count());
         }
     }
 }
