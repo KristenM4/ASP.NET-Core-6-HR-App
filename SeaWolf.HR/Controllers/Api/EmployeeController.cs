8a843ac529e5179134db4266fe99b50c21e0063a
KristenM4 <kristen4810@gmail.com>
2023-07-10 10:10:28 +0800

    create employeecontroller and locationcontroller classes

---
+++ SeaWolf.HR/Controllers/Api/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeaWolf.HR.Models;
+
+namespace SeaWolf.HR.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(IEmployeeRepository employeeRepository, ILogger<EmployeeController> logger)
+        {
+            _employeeRepository = employeeRepository;
+            _logger = logger;
+        }
+    }
+}
---
+++ SeaWolf.HR/Controllers/Api/LocationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeaWolf.HR.Models;
+
+namespace SeaWolf.HR.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly ILocationRepository _locationRepository;
+        private readonly ILogger<LocationController> _logger;
+
+        public LocationController(ILocationRepository locationRepository, ILogger<LocationController> logger)
+        {
+            _locationRepository = locationRepository;
+            _logger = logger;
+        }
+    }
+}
