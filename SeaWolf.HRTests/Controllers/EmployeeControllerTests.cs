cd532bcdbd72976400ecb342c52bd665bd45a524
KristenM4 <kristen4810@gmail.com>
2023-07-10 10:24:44 +0800

    create employeecontrollertests class

---
+++ SeaWolf.HRTests/Controllers/EmployeeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SeaWolf.HR.Controllers.Api;
+using SeaWolf.HR.Models;
+using SeaWolf.HRTests.Mocks;
+
+namespace SeaWolf.HR.Controllers
+{
+    public class EmployeeControllerTests
+    {
+        private readonly Mock<IEmployeeRepository> _mockEmployeeRepository;
+        private readonly EmployeeController _controller;
+
+        public EmployeeControllerTests()
+        {
+            _mockEmployeeRepository = RepositoryMocks.GetEmployeeRepository();
+            _controller = new EmployeeController(_mockEmployeeRepository.Object, Mock.Of<ILogger<EmployeeController>>());
+        }
+    }
+}
