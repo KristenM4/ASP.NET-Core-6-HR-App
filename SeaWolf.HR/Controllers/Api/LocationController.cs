c67d79f66ea71143f25f920b828f0780be30b2ab
KristenM4 <kristen4810@gmail.com>
2023-07-10 11:40:59 +0800

    add getalllocations and getlocationdetails methods to locationcontroller api

--- SeaWolf.HR/Controllers/Api/LocationController.cs
+++ SeaWolf.HR/Controllers/Api/LocationController.cs
@@ -15,5 +15,40 @@ namespace SeaWolf.HR.Controllers.Api
             _locationRepository = locationRepository;
             _logger = logger;
         }
+
+        [HttpGet]
+        public IActionResult GetAllLocations()
+        {
+            try
+            {
+                var allLocations = _locationRepository.AllLocations;
+                return Ok(allLocations);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get Location/GetAllLocations: {ex}");
+                return BadRequest("Failed to get all locationss from Location api");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetLocationDetails(int id)
+        {
+            try
+            {
+                var location = _locationRepository.GetLocationById(id);
+                if (location == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(location);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get Location/GetLocationDetails: {ex}");
+                return BadRequest("Failed to get location from Location api");
+            }
+        }
     }
 }
