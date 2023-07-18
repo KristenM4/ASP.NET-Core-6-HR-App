d74301880d83ea2cd74672d1c3f0d62e127378c4
KristenM4 <kristen4810@gmail.com>
2023-07-18 12:54:14 +0800

    create HRUser class for user model

---
+++ SeaWolf.HR/Models/HRUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SeaWolf.HR.Models
+{
+    public class HRUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
