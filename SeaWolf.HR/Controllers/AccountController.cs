d704dab95e88758637e3976fbe3925a16ff953db
KristenM4 <kristen4810@gmail.com>
2023-07-18 16:42:22 +0800

    add login methods for get and post

--- SeaWolf.HR/Controllers/AccountController.cs
+++ SeaWolf.HR/Controllers/AccountController.cs
@@ -1,14 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SeaWolf.HR.Models;
+using SeaWolf.HR.ViewModels;
 
 namespace SeaWolf.HR.Controllers
 {
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<HRUser> _signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger, SignInManager<HRUser> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "App");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password,
+                    model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "App");
+                    }
+                }
+            }
+
+            ModelState.AddModelError("", "Failed to log in");
+
+            return View();
         }
     }
 }
