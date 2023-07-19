d2b4a1db2af020687f93752684bff67d309e8d75
KristenM4 <kristen4810@gmail.com>
2023-07-19 12:21:23 +0800

    add createtoken method for generating jwt in the api

--- SeaWolf.HR/Controllers/AccountController.cs
+++ SeaWolf.HR/Controllers/AccountController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using SeaWolf.HR.Models;
 using SeaWolf.HR.ViewModels;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SeaWolf.HR.Controllers
 {
@@ -9,11 +13,18 @@ namespace SeaWolf.HR.Controllers
     {
         private readonly ILogger<AccountController> _logger;
         private readonly SignInManager<HRUser> _signInManager;
+        private readonly UserManager<HRUser> _userManager;
+        private readonly IConfiguration _config;
 
-        public AccountController(ILogger<AccountController> logger, SignInManager<HRUser> signInManager)
+        public AccountController(ILogger<AccountController> logger,
+            SignInManager<HRUser> signInManager,
+            UserManager<HRUser> userManager,
+            IConfiguration config)
         {
             _logger = logger;
             _signInManager = signInManager;
+            _userManager = userManager;
+            _config = config;
         }
 
         public IActionResult Login()
@@ -55,6 +66,48 @@ namespace SeaWolf.HR.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "App");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken([FromBody] LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.Username);
+
+                if (user != null)
+                {
+                    var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+                    if (result.Succeeded)
+                    {
+                        var claims = new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                            new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
+                        };
+
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+                        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                        var token = new JwtSecurityToken(
+                            _config["Tokens:Issuer"],
+                            _config["Tokens:Audience"],
+                            claims,
+                            signingCredentials: credentials,
+                            expires: DateTime.UtcNow.AddMinutes(60));
+
+                        return Created("", new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            expiration = token.ValidTo
+                        });
+                    }
+                }
+            }
+
+            return BadRequest();
         }
     }
 }
