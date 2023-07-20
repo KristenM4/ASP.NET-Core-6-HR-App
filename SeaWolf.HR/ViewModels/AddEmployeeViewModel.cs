e36edb5cb860810b7fb7748b6ed036ee9fd7defd
KristenM4 <kristen4810@gmail.com>
2023-07-20 10:16:39 +0800

    create addemployeeviewmodel and link in addemployee page

---
+++ SeaWolf.HR/ViewModels/AddEmployeeViewModel.cs
@@ -0,0 +1,34 @@
+﻿using SeaWolf.HR.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace SeaWolf.HR.ViewModels
+{
+    public class AddEmployeeViewModel
+    {
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string? MiddleName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Position { get; set; }
+
+        [Required]
+        public Location? Location { get; set; }
+    }
+}
