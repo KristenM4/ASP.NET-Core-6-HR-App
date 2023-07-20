484d2fed17598daa5d10714d32eb83d4b0a73d76
KristenM4 <kristen4810@gmail.com>
2023-07-20 16:31:23 +0800

    require authorization for addemployee page

--- SeaWolf.HR/Controllers/AppController.cs
+++ SeaWolf.HR/Controllers/AppController.cs
@@ -69,6 +69,7 @@ namespace SeaWolf.HR.Controllers
             }
         }
 
+        [Authorize]
         public IActionResult AddEmployee()
         {
             try
@@ -85,6 +86,7 @@ namespace SeaWolf.HR.Controllers
             }
         }
 
+        [Authorize]
         [HttpPost]
         public IActionResult AddEmployee(AddEmployeeViewModel model)
         {
