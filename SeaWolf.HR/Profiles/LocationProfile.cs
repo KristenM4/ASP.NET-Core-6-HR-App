6033725d75ac4fb3e830737ffb8e540f73b48eb6
KristenM4 <kristen4810@gmail.com>
2023-07-27 16:34:35 +0800

    create automapper profile for location api

---
+++ SeaWolf.HR/Profiles/LocationProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SeaWolf.HR.Models;
+using SeaWolf.HR.ViewModels;
+
+namespace SeaWolf.HR.Profiles
+{
+    public class LocationProfile : Profile
+    {
+        public LocationProfile()
+        {
+            CreateMap<AddLocationViewModel, Location>();
+        }
+    }
+}
