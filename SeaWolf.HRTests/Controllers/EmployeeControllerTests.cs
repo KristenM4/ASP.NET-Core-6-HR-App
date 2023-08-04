8bbb26540fc96a8be4a83f7f72ec1580fef4650f
KristenM4 <kristen4810@gmail.com>
2023-08-04 16:48:54 +0800

    tests added for UpdateEmployee and PartiallyUpdateEmployee

--- SeaWolf.HRTests/Controllers/EmployeeControllerTests.cs
+++ SeaWolf.HRTests/Controllers/EmployeeControllerTests.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using SeaWolf.HR.Controllers.Api;
+using SeaWolf.HR.Mocks;
 using SeaWolf.HR.Models;
 using SeaWolf.HR.ViewModels;
 using SeaWolf.HRTests.Mocks;
@@ -19,8 +21,8 @@ namespace SeaWolf.HR.Controllers
 
         public EmployeeControllerTests()
         {
-            _mockEmployeeRepository = RepositoryMocks.GetEmployeeRepository();
-            _mockLocationRepository = RepositoryMocks.GetLocationRepository();
+            _mockEmployeeRepository = EmployeeRepositoryMock.GetEmployeeRepository();
+            _mockLocationRepository = LocationRepositoryMock.GetLocationRepository();
             
             _controller = new EmployeeController(_mockEmployeeRepository.Object, _mockLocationRepository.Object, Mock.Of<ILogger<EmployeeController>>());
 
@@ -79,6 +81,36 @@ namespace SeaWolf.HR.Controllers
 
             var actionResult = Assert.IsAssignableFrom<ActionResult<Employee>>(result);
             Assert.IsType<CreatedAtRouteResult>(actionResult.Result);
+        }
+
+        [Fact]
+        public void UpdateEmployee_Changes_Employee_Details()
+        {
+            var updatedEmployeeInfo = new UpdateEmployeeViewModel()
+            {
+                FirstName = "New",
+                LastName = "Employee",
+                DateOfBirth = new DateTime(1999, 01, 01),
+                Email = "nemployee@email.com",
+                Phone = "1234567895",
+                Position = "Tester",
+                Location = "Farrington Store"
+            };
+            var result = _controller.UpdateEmployee(1, updatedEmployeeInfo);
+            var employee = _mockEmployeeRepository.Object.GetEmployeeById(1);
+
+            Assert.IsType<NoContentResult>(result);
+            Assert.Equal("New", employee.FirstName);
+        }
+
+        [Fact]
+        public void PartiallyUpdateEmployee_Changes_Some_Employee_Details()
+        {
+            var jsonPatch = new JsonPatchDocument<UpdateEmployeeViewModel>();
+            jsonPatch.Replace(e => e.LastName, "patchName");
+            var result = _controller.PartiallyUpdateEmployee(1, jsonPatch);
+
+            Assert.IsType<NoContentResult>(result);
         }
     }
 }
