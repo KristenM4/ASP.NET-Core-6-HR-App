1f85c568f0310a0856623f4aad9a365c5a5a1b68
KristenM4 <kristen4810@gmail.com>
2023-08-25 23:54:36 +0800

    add XML comments for all methods in employee repository

--- SeaWolf.HR/Models/EmployeeRepository.cs
+++ SeaWolf.HR/Models/EmployeeRepository.cs
@@ -11,14 +11,24 @@ namespace SeaWolf.HR.Models
             _context = seaWolfHRDbContext;
         }
 
+        /// <summary>
+        /// Get all employees in the database
+        /// </summary>
+        /// <returns>IEnumerable of Employees</returns>
         public IEnumerable<Employee> AllEmployees => _context.Employees.
             Include(e=>e.Location).OrderBy(e => e.LastName).ThenBy(e => e.FirstName);
 
+        /// <summary>
+        /// Add a new employee to the database
+        /// </summary>
         public void AddEmployee(Employee employee)
         {
             _context.Employees.Add(employee);
         }
 
+        /// <summary>
+        /// Delete an employee from the database
+        /// </summary>
         public void DeleteEmployee(int employeeId)
         {
             var employee = GetEmployeeById(employeeId);
@@ -29,16 +39,28 @@ namespace SeaWolf.HR.Models
             }
         }
 
+        /// <summary>
+        /// Get a specific employee from the database
+        /// </summary>
+        /// <returns>An Employee object</returns>
         public Employee? GetEmployeeById(int employeeId)
         {
             return _context.Employees.Include(e => e.Location).FirstOrDefault(e => e.EmployeeId == employeeId);
         }
 
+        /// <summary>
+        /// Save changes to the database
+        /// </summary>
+        /// <returns>True if any changes were saved, false if no changes were saved</returns>
         public bool Save()
         {
             return _context.SaveChanges() > 0;
         }
 
+        /// <summary>
+        /// Keyword search and sort for employees
+        /// </summary>
+        /// <returns>IEnumerable of Employees</returns>
         public IEnumerable<Employee> SearchEmployees(string searchQuery)
         {
             if (string.IsNullOrWhiteSpace(searchQuery))
@@ -60,6 +82,10 @@ namespace SeaWolf.HR.Models
 
         // ASYNC
 
+        /// <summary>
+        /// Get all employees in the database. This is an async method.
+        /// </summary>
+        /// <returns>Async Task: IEnumerable of Employees</returns>
         public async Task<IEnumerable<Employee>> AllEmployeesAsync()
         {
             return await _context.Employees.
@@ -67,11 +93,10 @@ namespace SeaWolf.HR.Models
             .ToListAsync();
         }
 
-        public void AddEmployeeAsync(Employee employee)
-        {
-            _context.Employees.Add(employee);
-        }
-
+        /// <summary>
+        /// Save changes to the database asynchronously
+        /// </summary>
+        /// <returns>Task with result of how many changes were saved</returns>
         public async Task SaveAsync()
         {
             await _context.SaveChangesAsync();
